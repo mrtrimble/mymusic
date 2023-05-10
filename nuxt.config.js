d3a1955e94eba35e40530d73c090e716156d5c3f
Ryan Trimble <mrtrimble2@gmail.com>
2023-05-10 18:22:33 -0400

    updated preset

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase', '@pinia/nuxt', '@nuxtjs/google-fonts'],
   css: ['@acab/reset.css', '~/assets/css/main.scss'],
   buildModules: [],
+  preset: 'netlify-edge',
   env: {
     discogKey: process.env.DISCOG_KEY,
     discogSecret: process.env.DISCOG_SECRET,
@@ -10,7 +11,7 @@ export default defineNuxtConfig({
   googleFonts: {
     families: {
       'Josefin+Sans': [400, 600, 900],
-      Figtree: [400, 600, 700]
+      Figtree: [400, 600, 700],
     },
     display: 'swap',
     prefetch: true,
